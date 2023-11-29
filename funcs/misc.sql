4c2203314f00bbc063a4138c6ae341aeb45f84c2
kru01 <79639749+kru01@users.noreply.github.com>
2023-11-29 20:39:32 +0700

    v0.5.0 - wrote db creation script - wrote func to generate IDs - wrote insert stProcs for all tables - wrote batch script to setup db - created temp data folder

---
+++ funcs/misc.sql
@@ -0,0 +1,22 @@
+﻿/* 21HTTT1 - 21CLC1.CSDLNC.03
+ * 21127004 - Trần Nguyễn An Phong
+ * 21127135 - Diệp Hữu Phúc
+ * 21127296 - Đặng Hà Huy
+ * 21127315 - Nguyễn Gia Khánh
+ * 21127712 - Lê Quang Trường
+ */
+USE [NC03_ORDERENTRY]
+GO
+
+CREATE OR ALTER FUNCTION F_MAKE_ID
+    (@TYPE CHAR(2), @ID CHAR(7))
+RETURNS CHAR(7) AS BEGIN
+    IF (@ID IS NULL) SET @ID = @TYPE + '00001'
+    ELSE BEGIN
+        SET @ID = CAST(RIGHT(@ID, 5) AS INT) + 1
+        SET @ID = @TYPE + RIGHT(RTRIM('00000'
+            + CAST(@ID AS CHAR(5))), 5)
+    END
+    RETURN @ID
+END
+GO
